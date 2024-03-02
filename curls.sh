9cc943ef6b4c555a3d703ea3a11d22eeaddd38ec
root <dvinaja15798@plataforma-utslp.net>
2024-03-01 18:51:13 -0600

    miprimerarchivo

--- curls.sh
+++ curls.sh
@@ -6,22 +6,32 @@ urls=(
     "www.google.com"
     "www.youtube.com"
     "www.spotify.com"
-    "www.hbo.com"
+    "www.8979798.com"
     "www.facebook.com"
-    "www.telmex.com"
-    "www.hjvjhvjhvhjvi.com"
+    "www.ruralnet.com"
+    "www.hyundai.com"
     "www.whatsapp.com"
     "www.xvideos.com"
 )
+# Función para imprimir en verde
+print_ok() {
+    echo -e "\e[32mOK\e[0m"
+}
 
-echo "      Done          |        Fail"
-echo "----------------------+------------------"
+# Función para imprimir en rojo
+print_fail() {
+    echo -e "\e[31mFail\e[0m"
+}
+
+echo "Página Web               | OK  | Fail"
+echo "-------------------------+-----+-----"
 
 # Recorre la lista de URLs y realiza solicitudes CURL
 for url in "${urls[@]}"; do
     if curl -s --head "$url" | head -n 1 | grep "HTTP/1.[01] [23].." > /dev/null; then
-        echo "$url |"
+        echo -n "$url | $(print_ok)  |"
     else
-        echo "| $url"
+        echo -n "$url |      | $(print_fail)"
     fi
+    echo ""
 done
